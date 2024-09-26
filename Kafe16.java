f5eb761124eab5d25dbaa4fcf2961ee04862b4f0
M-Aqilaz <muhaaqil6002@gmail.com>
2024-09-26 07:45:33 +0700

    commit 1

---
+++ Kafe16.java
@@ -0,0 +1,54 @@
+
+import java.util.Scanner;
+
+public class Kafe16 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        String menu;
+        char ukuranCup;
+        int jumlah;
+        boolean keanggotaan;
+
+        System.out.print("Masukkan Menu: ");
+        menu= sc.nextLine();
+        System.out.print("Masukkan ukuran cup: ");
+        ukuranCup= sc.next().charAt(0);
+        System.out.print("Masukkan Jumlah: ");
+        jumlah= sc.nextInt();
+        System.out.print("Masukkan Keanggotaan: ");
+        keanggotaan= sc.nextBoolean();
+
+        double hargaMenu= 0;
+
+        switch (menu.toLowerCase()) {
+            case "kopi":
+                hargaMenu=12_000;
+                break;
+            case "teh":
+                hargaMenu=7_000;
+                break;
+            case "coklat":
+                hargaMenu=20_000;
+                break;
+        }
+        double totalHarga= hargaMenu*jumlah;
+
+        switch (ukuranCup) {
+            case 'S':
+                break;
+            case 'M':
+                totalHarga+=0.25*totalHarga;
+                break;
+            case 'L':
+                totalHarga+=0.4*totalHarga;
+                break;
+        }
+        double diskon=keanggotaan? 0:0.1;
+        double nominalBayar= totalHarga-(diskon*totalHarga);
+
+        System.out.println("Item pembelian: " + jumlah + "" + menu +" dengan ukuran cup " + ukuranCup);
+        System.out.println("Nominal bayar: " + nominalBayar);
+    }
+    
+}
